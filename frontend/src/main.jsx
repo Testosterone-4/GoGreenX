da285b3531e2074531b89bdbfd2e08c88c9003df
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-04-15 22:11:26 +0200

    modified the main.jsx to add the GoogleOAuthProvider tag

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="354038241485-rueb7dvachumoq31vj6s6hq9l50b2u5a.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
