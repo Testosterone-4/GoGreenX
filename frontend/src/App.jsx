7a549274cf4f8e068da269220faad2803875d08c
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-04-18 16:41:38 +0200

    applied the rout for the training.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
-import Home from './pages/Home.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-import Profile from './pages/Profile.jsx';
-import Dashboard from './pages/Dashboard.jsx';
-import Actions from './pages/Actions.jsx';
-import FitnessPlan from './pages/FitnessPlan.jsx';
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Profile from "./pages/Profile.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Actions from "./pages/Actions.jsx";
+import FitnessPlan from "./pages/FitnessPlan.jsx";
+import Training from "./pages/Training.jsx";
 
 function App() {
   return (
@@ -23,6 +24,9 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/actions" element={<Actions />} />
           <Route path="/fitness-plan" element={<FitnessPlan />} />
+          <Route path="/training" element={<Training />} />
+          {/* <Route path="/nutrition" element={<Nutrition />} />
+          <Route path="/forum" element={<Forum />} /> */}
         </Routes>
       </div>
       <Footer />
