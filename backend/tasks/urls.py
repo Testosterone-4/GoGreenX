2f129344f6a34a1821d29a333893541ec5ddf38a
Mohgar <mohamedhgar007@gmail.com>
2025-04-27 21:08:39 +0300

    add some styles to the ui

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('plan/', FitnessPlanView.as_view(), name='fitness-plan'),       # /api/tasks/plan/
     path('list/', TaskListView.as_view(), name='task-list'),             # /api/tasks/list/
     path('list/<uuid:task_id>/', TaskUpdateView.as_view(), name='task-update'),  # /api/tasks/list/{task_id}/
+    path('delete/<uuid:task_id>/', TaskDeleteView.as_view(), name='task-delete'),
 ]
